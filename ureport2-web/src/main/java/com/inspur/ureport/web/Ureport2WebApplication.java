e33e16b8fb3f81757c20eff3228af685e215c979
hqs_1992 <hqs_1992@163.com>
2019-10-08 09:50:21 +0800

    添加web模块，便于springboot启动测试

---
+++ ureport2-web/src/main/java/com/inspur/ureport/web/Ureport2WebApplication.java
@@ -0,0 +1,12 @@
+package com.inspur.ureport.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ureport2WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ureport2WebApplication.class, args);
+    }
+}
